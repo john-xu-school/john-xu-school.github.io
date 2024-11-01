075e4abb24554f3617745ea713e54f1e7deba89e
john-xu-school <63273190+john-xu-school@users.noreply.github.com>
2024-11-01 02:02:58 -0700

    pre beach blitz

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "338317752"
+    serviceWorkerVersion: "2907645370"
   }
 });
